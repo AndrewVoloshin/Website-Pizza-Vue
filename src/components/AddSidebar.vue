1042a9925d93ab75edda53a1c18cb62844ef189a
Andrew Voloshin <voloshinandy@gmail.com>
2022-06-06 14:47:49 +0300

    add: sidebar transition, header navigation

--- src/components/AddSidebar.vue
+++ src/components/AddSidebar.vue
@@ -1,25 +1,27 @@
 <template>
+<div>
   <div class="sidebar__overlay " 
-  v-if="isActiveSidebar"
-  @click="$store.state.isActiveSidebar = false"
-  @click.stop
+    @click="$store.state.isActiveSidebar = false"
+    :class="{'sidebar__overlay_on':$store.state.isActiveSidebar} "
+  ></div>
+  <div class="sidebar"
+  :class="{'sidebar_on':$store.state.isActiveSidebar}"
   >
-    <div class="sidebar">
-      <div class="sidebar__conteiner">
-        <a class="sidebar__logo">
-          <img src="@/assets/Logo.png" alt="logo">
-        </a>
-        <nav>
-          <ul class="sidebar__menu">
-            <router-link class="menu__item" tag="li" to="/" exact active-class="menu__item_on" >Home</router-link>
-            <router-link class="menu__item" tag="li" to="/menu" exact active-class="menu__item_on">Menu</router-link>
-            <router-link class="menu__item" tag="li" to="/login" exact active-class="menu__item_on">Log In</router-link>
-            <router-link class="menu__item" tag="li" to="/register" exact active-class="menu__item_on">Register</router-link>
-          </ul>
-        </nav>
-      </div> 
-    </div>
+    <div class="sidebar__conteiner">
+      <a class="sidebar__logo">
+        <img src="@/assets/Logo.png" alt="logo">
+      </a>
+      <nav>
+        <ul class="sidebar__menu">
+          <router-link class="menu__item" tag="li" to="/" exact active-class="menu__item_on" >Home</router-link>
+          <router-link class="menu__item" tag="li" to="/menu" exact active-class="menu__item_on">Menu</router-link>
+          <router-link class="menu__item" tag="li" to="/login" exact active-class="menu__item_on">Log In</router-link>
+          <router-link class="menu__item" tag="li" to="/register" exact active-class="menu__item_on">Register</router-link>
+        </ul>
+      </nav>
+    </div> 
   </div>
+</div>
 </template>
 
 <script>
@@ -34,18 +36,32 @@ export default {
 
 <style scoped>
 .sidebar__overlay{
+  visibility: hidden;
   position:absolute;
   top:0px;
   left:0px;
-  background: 	rgb(0,0,0, 0.5);
+  background:rgb(0,0,0, 0.5);
   height: 100vh;
   width: 100vw;
 }
+
+.sidebar__overlay_on{
+  visibility: visible;
+}
+
 .sidebar{
+  position:absolute;
+  top:0px;
+  left:-40vw;
   width: 40vw;
   height: 100vw;
   background: white;
   padding: 0 16px;
+  transition: left .2s;
+}
+.sidebar_on{
+  left:0vw;
+  transition: left .2s;
 }
 .sidebar__logo{
   position: relative;
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,16 @@
         <img src="@/assets/Logo.png" alt="logo">
       </a>
     <div class="header__burger" 
-    @click=" $store.state.isActiveSidebar = !$store.state.isActiveSidebar "
-    :class=" {'header__burger_on':$store.state.isActiveSidebar} " 
-    >
+      @click=" $store.state.isActiveSidebar = !$store.state.isActiveSidebar "
+      :class=" {'header__burger_on':$store.state.isActiveSidebar} " 
+      >
       <span :class="{'center-line': !$store.state.isActiveSidebar}"></span>
+    </div>
+    <div class="header__nav">
+      <router-link class="nav__link" tag="a" to="/" exact active-class="nav__link_on" >Home</router-link>
+      <router-link class="nav__link" tag="a" to="/menu" exact active-class="nav__link_on">Menu</router-link>
+      <router-link class="nav__link" tag="a" to="/login" exact active-class="nav__link_on">Log In</router-link>
+      <router-link class="nav__link" tag="a" to="/register" exact active-class="nav__link_on">Register</router-link>
     </div>
   </div>
   </div>
@@ -30,16 +36,17 @@ export default {
   position:fixed;
   top:0;
   left:0;
+  display:flex;
+  justify-content: center;
   background-color: #f0f0f0;
-  width:100vw;
+  width:100%;
   height: 56px;
 }
 .header__container{
   display: flex;
-  min-width: 720px;
   padding: 0 16px;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 }
 .header__logo{
   position: relative;
@@ -57,10 +64,10 @@ img{
   height: 100%;
 }
 .header__burger{
-  margin-left:563px;
   width:28px;
   height: 18px;
   position: relative;
+  padding: 1px 6px;
 }
 .center-line{
   width:100%;
@@ -90,10 +97,58 @@ img{
 .header__burger_on::after{
   transform: rotate(45deg) translate(-8px,-4px);
 }
+.header__nav{
+  display:none;
+  margin: 16px 0 16px 586px;
+  flex-wrap:nowrap;
+}
+
+.nav__link{
+  text-decoration: none;
+  margin: 0 8px;
+  width: max-content;
+  font-weight: 700;
+  color: #724cf9;
+  /* white-space: nowrap; */
+}
+
+.nav__link_on{
+  position:relative;
+  color: #0041bb;
+}
+.nav__link_on:after{
+  position:absolute;
+  left:0;
+  bottom:0;
+  content:"";
+  height: 2px;
+  width:100%;
+  background: #0041bb;
+}
+
+
 
 @media screen and (min-width: 768px) {
   .header__container{
-    min-width: 720px;
+    width: 720px;
+  }
+}
+
+@media screen and (min-width:992px){
+  .header__container{
+    width:960px;
+  }
+  .header__burger{
+    display:none;
+  }
+  .header__nav{
+    display:flex;
+  }
+}
+
+@media screen and (min-width:1200px){
+  .header__container{
+    width:1140px;
   }
 }
 
