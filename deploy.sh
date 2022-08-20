96483cfd3b208e25479c74d1ac835823c9b1ddf6
AndrewVoloshin <voloshinandy@gmail.com>
2022-08-20 13:45:07 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # если вы публикуете по адресу https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-если вы публикуете по адресу https://andrewvoloshin.github.io/Website-on-Vue
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# если вы публикуете по адресу https://andrewvoloshin.github.io/Website-on-Vue
+git push -f git@github.com:andrewvoloshin/Website-on-Vue.git master:gh-pages
 
 cd -
